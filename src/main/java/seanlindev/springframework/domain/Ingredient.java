1e06448f8516638b75453ef9896397914d12294b
Sean Lin <seanlin1016@gmail.com>
2021-05-18 14:29:45 +0800

    use setter for bidirectional relationship

--- src/main/java/seanlindev/springframework/domain/Ingredient.java
+++ src/main/java/seanlindev/springframework/domain/Ingredient.java
@@ -23,11 +23,10 @@ public class Ingredient {
 
     }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
         this.description = description;
         this.amount = amount;
         this.unitOfMeasure = uom;
-        this.recipe = recipe;
     }
 
     public long getId() {
